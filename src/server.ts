be56027e3d71d9b72b942fe20eb5a10a0811475e
CaioRibeiro-Dev <caio.ribeiro_dev.93@outlook.com>
2026-04-21 01:49:22 -0300

    os ajustes finais

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+console.log("URL DO BANCO NO SERVIDOR:", process.env.DATABASE_URL ? "Carregada com sucesso" : "VARIÁVEL VAZIA!");
+
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
@@ -6,6 +8,8 @@ import videoRoutes = require('./routes/video.routes');
 
 
 config(); // Carrega as variáveis do arquivo .env
+
+console.log("DEBUG - DATABASE_URL carregada:", process.env.DATABASE_URL);
 
 const app = express();
 
